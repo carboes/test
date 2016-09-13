9524c2f00ac142e8678590fd26f05659494abf85
carboes <carboes@gmail.com>
2016-09-14 09:57:41 +1200

    updated configs

--- config-sauce.js
+++ config-sauce.js
@@ -8,7 +8,7 @@ exports = {
         platformVersion: '9.3',
         platformName: 'iOS',
         name: 'LottoNZ SIT',
-        app: path.join(__dirname, 'apps/LottoNZ-SIT-ios.zip'),
+        app: 'sauce-storage:LottoNZ-SIT-ios.zip',
         autoAcceptAlerts: true
     },
     android: {
@@ -18,7 +18,7 @@ exports = {
         platformVersion: '4.4.4',
         platformName: 'Android',
         name: 'LottoNZ SIT',
-        app: path.join(__dirname, 'apps/mylotto-sit.apk'),
+        app: 'sauce-storage:LottoNZ-SIT-android.zip',
         appActivity: 'co.nz.mylotto.MainActivity'
     }
 }
